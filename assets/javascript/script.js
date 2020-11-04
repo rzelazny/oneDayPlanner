b84fbb3caac72ff8ed6be9b6ccde4acde32a36ec
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-04 17:39:30 -0500

    working local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,10 @@
 
 // set Day
-var today = moment().format('ll');
-$("#currentDay").text(today);
+$("#currentDay").text(moment().format('ll'));
 
 //get timeblock to set their colors
-
-var timeBlocks = $(".description");
-//var timeBlocks = document.getElementsByClassName("description");
+timeBlocks = $(".description");
+    //var timeBlocks = document.getElementsByClassName("description");
 
 //current time block in 24 hour time
 var curTimeBlock = (moment().format('H'));
@@ -33,6 +31,47 @@ for (var i = 0; i< timeBlocks.length; i++){
     }
 }
 
+//store and retrieve saved events to local storage
+var events = [];
+saveButtons = $(".saveBtn");
+
+function init() {
+    // Parsing the JSON string to an object
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+
+    // If events were retrieved from localStorage, update the event array to it
+    if (storedEvents !== null) {
+        events = storedEvents;
+
+        //set the stored events
+        for (i=0; i < events.length; i++){
+            if(events[i].event !== null){
+                timeBlocks[storedEvents[i].timeblock].innerHTML = storedEvents[i].event;
+            }
+        }
+    }
+}
+
+$(".saveBtn").on("click", function(){
+
+    //console.log($(this));
+   // console.log(timeBlocks[0].value);
+
+    var newEvent = {
+        timeblock: this.attributes.timeindex.value,
+        event: timeBlocks[this.attributes.timeindex.value].value
+    }
+
+    //console.log(newEvent);
+    events.push(newEvent);
+
+    //console.log(events);
+
+    localStorage.setItem("events", JSON.stringify(events));
+})
+
+//always run initialization code
+init();
 
 
 
